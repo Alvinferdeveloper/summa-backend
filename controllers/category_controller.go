41944d14eb4d495ec716222d9a7e254c3923132d
Albin <alvinfer67@gmail.com>
2025-10-12 23:00:24 -0600

    feat(category): convert category to response dto when getting categories

--- controllers/category_controller.go
+++ controllers/category_controller.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/Alvinferdeveloper/summa-backend/config"
+	"github.com/Alvinferdeveloper/summa-backend/dto"
 	"github.com/Alvinferdeveloper/summa-backend/models"
 	"github.com/gin-gonic/gin"
 )
@@ -14,5 +15,9 @@ func GetCategories(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al obtener las categorías"})
 		return
 	}
-	c.JSON(http.StatusOK, categories)
+	var categoriesDTO []*dto.CategoryResponseDTO
+	for _, category := range categories {
+		categoriesDTO = append(categoriesDTO, dto.ConvertCategoryToDTO(category))
+	}
+	c.JSON(http.StatusOK, categoriesDTO)
 }
